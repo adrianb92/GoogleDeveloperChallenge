58a2f2acffbc5edc0df42226c0bc1646665a87c9
Adrian <adrian.barczuk@gmail.com>
2018-01-21 22:41:50 +0100

    feat: create grid and set background color

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
-// Select color input
-// Select size input
+let height;
+let width;
+let color;
+let cells;
+let content;
 
-// When size is submitted by the user, call makeGrid()
+$(':submit').on('click', function(evt){
+  evt.preventDefault();
+  $('table').html('');
+  makeGrid();
+});
 
 function makeGrid() {
+  height = $('#input_height').val();
+  width = $('#input_width').val();
+  content = "";
+  for (let  i = 0; i < height && i <= 100; i++){
+    content += "<tr>"
+    for (let j = 0; j < width && j <=100; j++){
+      content += "<td></td>";
+    }
+    content += "</tr>";
+  }
+  $('#pixel_canvas').append(content);
+  cells = $('td');
+  Array.prototype.forEach.call(cells, function(cell) {
+      cell.addEventListener('click', function() {
+        let color = $('#colorPicker').val();
+        $(this).attr("bgcolor", color);
 
-// Your code goes here!
-
+    })})
 }
