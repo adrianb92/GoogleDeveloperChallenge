c34ae3194305946651302c247178ed16e2d79267
Adrian <adrian.barczuk@gmail.com>
2018-01-29 22:57:37 +0100

    feat: dblclick remove color, color by dragging mouse, download image

--- designs.js
+++ designs.js
@@ -1,32 +1,54 @@
-let height;
-let width;
-let color;
-let cells;
-let content;
-
-$(':submit').on('click', function(evt){
-  evt.preventDefault();
+$('form').submit(function(event){
   $('table').html('');
   makeGrid();
+  event.preventDefault();
 });
 
 function makeGrid() {
-  height = $('#input_height').val();
-  width = $('#input_width').val();
-  content = "";
-  for (let  i = 0; i < height && i <= 100; i++){
+  let height = $('#input_height').val();
+  let width = $('#input_width').val();
+  let content = "";
+  for (let  i = 0; i < height; i++){
     content += "<tr>"
-    for (let j = 0; j < width && j <=100; j++){
+    for (let j = 0; j < width; j++){
       content += "<td></td>";
     }
     content += "</tr>";
   }
   $('#pixel_canvas').append(content);
-  cells = $('td');
+  let cells = $('td');
+  let color;
   Array.prototype.forEach.call(cells, function(cell) {
-      cell.addEventListener('click', function() {
-        let color = $('#colorPicker').val();
+    cell.addEventListener('mousedown', function() {
+      color = $('#colorPicker').val();
+      $(this).attr("bgcolor", color);
+    })
+    cell.addEventListener('dragstart', function(evt) {
+      evt.preventDefault();
+    })
+    cell.addEventListener('mouseover', function(evt) {
+      if (evt.buttons == 1) {
+        color = $('#colorPicker').val();
         $(this).attr("bgcolor", color);
+      }
+    })
+    cell.addEventListener('dblclick', function(evt) {
+        $(this).attr("bgcolor", "white");
+    })
+  })
+}
 
-    })})
+$('#download_btn').click(function() {
+  html2canvas(document.querySelector("#pixel_canvas")).then(canvas => {
+    saveAs(canvas.toDataURL(), 'canvas.png');
+  })
+})
+
+function saveAs(uri, filename) {
+    var link = document.createElement('a');
+    link.href = uri;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
